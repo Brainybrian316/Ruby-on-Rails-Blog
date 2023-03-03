702b07e3d76a8b931834f8d3875009c4e88b25f5
Brainybrian316 <brainybrian316@gmail.com>
2023-03-02 22:33:44 -0500

    add article listing features 86

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,5 +2,9 @@ class ArticlesController < ApplicationController
   def show
     # @variable is an instance variable
     @article = Article.find(params[:id]) # params is a hash provided by Rails
-  end 
-end 
+  end
+
+  def index
+    @articles = Article.all
+  end
+end
